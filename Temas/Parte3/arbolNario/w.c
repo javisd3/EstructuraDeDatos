a90789d75b24fd50310d40a971b1b5c883e02a62
Javisd3 <javier.smh06@gmail.com>
2026-04-20 14:33:00 +0200

    sfs

--- Temas/Parte3/arbolNario/w.c
+++ Temas/Parte3/arbolNario/w.c
@@ -1,7 +1,21 @@
+//como hijo-hermano
 typedef struct Nodo{
     tElemento info;
     struct Nodo *hijo;
     struct Nodo *hermano;
+}tNodo;
+
+typedef tNodo *tArbol;
+
+//como lista de hijos
+typedef struct nodo{
+    struct Nodo *ref;;
+    struct Hijo *sig;
+}tHijo;
+
+typedef struct Nodo{
+    tElemento info;
+    tHijo *tListaHijos;
 }tNodo;
 
 typedef tNodo *tArbol;
